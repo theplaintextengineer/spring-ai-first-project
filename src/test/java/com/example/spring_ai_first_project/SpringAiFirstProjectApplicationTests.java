1e62df640f392c3e8a768ba45b84304c03835a61
theplaintextengineer <akshay2104.singh@gmail.com>
2026-01-28 10:07:10 +0530

    Init Commit

---
+++ src/test/java/com/example/spring_ai_first_project/SpringAiFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_ai_first_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
