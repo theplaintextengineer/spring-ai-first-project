ec7dfdb1367a506f84d9f1f37dbe238575330d53
theplaintextengineer <akshay2104.singh@gmail.com>
2026-01-28 10:07:10 +0530

    Init Commit

---
+++ src/main/java/com/example/spring_ai_first_project/SpringAiFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_ai_first_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiFirstProjectApplication.class, args);
+	}
+
+}
