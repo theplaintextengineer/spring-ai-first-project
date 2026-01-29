d52316e024b739bb5694d5562623e954587ede29
The Plaintext Engineer <akshay2104.singh@gmail.com>
2026-01-29 03:39:23 +0000

    Added Vaadin UI

--- src/main/java/com/example/spring_ai_first_project/controller/AiController.java
+++ src/main/java/com/example/spring_ai_first_project/controller/AiController.java
@@ -4,10 +4,12 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path="/api/ai")
 public class AiController {
   private final ChatClient chatClient;
 
